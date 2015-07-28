55fedd95cfb934999e12f2c27c2f27a2a1c150a0
Steve Forkin <steve.forkin@gmail.com>
2015-07-28 21:54:37 +1000

    adding some documentation and tidied up the form_helper

--- spec/railtie/railtie_spec.rb
+++ spec/railtie/railtie_spec.rb
@@ -6,7 +6,7 @@ require 'mustache_form'
 require 'spec_helper_load_application'
 #
 describe MustacheForm::Railtie do
-  it 'after initialisation the App should have the listly constants module configured' do
+  it 'after initialisation the App should have the mustache_form constants module configured' do
     expect(Rails.application.config.mustache_form[:simple_form_enabled]).to eq true
   end
 end
