e19a0b99cc15f9b8946c2404dd3c351317ee236a
Steve Forkin <steve.forkin@gmail.com>
2015-08-02 12:40:22 +1000

    fixed a bug in the railtie that turned up after production deploy

--- lib/mustache_form/railtie.rb
+++ lib/mustache_form/railtie.rb
@@ -1,14 +1,13 @@
 #
 module MustacheForm
   class Railtie < Rails::Railtie
-    config.eager_load_namespaces << MustacheForm
 
     config.after_initialize do
       unless MustacheForm.configured?
         warn 'Mustache Form is not configured in the application and will use the default values.' +
           ' Use `rails generate mustache_form:install` to generate the Mustache Form configuration.'
       end
-    end  
+    end
   end
 end
 #
